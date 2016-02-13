96e7ad14e85e470c8de09506fcf2695851931c8d
yzyDavid <yzyDavid@qq.com>
2016-02-13 22:52:48 +0800

    TODO: work out codec problem with url request

--- src/hj_dict/search_word.py
+++ src/hj_dict/search_word.py
@@ -1,5 +1,11 @@
-import urllib.request
+import urllib.request as ur
+import codecs
 
 
 def search_word(word):
-    pass
+    basic_url = r'http://dict.hjenglish.com/jp/jc/'
+    search_url = basic_url + word
+    #search_url = search_url.encode('ascii')
+    fp = ur.urlopen(search_url)
+    html_str = fp.read().decode('utf-8')
+    print(html_str)
---
+++ src/hj_dict/test.py
@@ -0,0 +1,3 @@
+from src.hj_dict.search_word import search_word
+
+search_word('勉強')
