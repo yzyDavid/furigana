c0fcee655e96bfaecdc07d399f62e6f54dd865fd
yzyDavid <yzyDavid@qq.com>
2016-02-17 21:30:49 +0800

    add S&L part !without testing

--- src/core/dict.py
+++ src/core/dict.py
@@ -30,6 +30,35 @@ def load(filename):
 
 
 def load_dict(last_num):
+    load_processed(r'../res/dict/dict_processed.txt')
     for i in range(0, last_num):
         name = '../res/dict' + '%03d' % i + '.txt'
         load(name)
+    save(r'../res/dict/dict_processed.txt')
+
+
+def save(filename):
+    fp = open(filename, 'w', encoding='utf-8')
+    for index in rep_dict:
+        fp.write(index)
+        fp.write(rep_dict[index])
+        fp.write()
+    fp.close()
+
+
+def load_processed(filename):
+    fp = open(filename, 'r', encoding='utf-8')
+    flag = 0
+    for line in fp:
+        if line.endswith('\n'):
+            line = line[0:-1]
+        if flag == 0:
+            match = str(line)
+        elif flag == 1:
+            sub = str(line)
+            rep_dict[match] = sub
+        elif flag == 2:
+            pass
+        flag += 1
+        flag %= 3
+    fp.close()
