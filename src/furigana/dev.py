9afeb09609eb3bcdb2fcb21c30a58303f9a5d9e8
yzyDavid <yzyDavid@qq.com>
2017-02-04 19:54:47 +0000

    finished bs html parse test

--- src/furigana/dev.py
+++ src/furigana/dev.py
@@ -5,10 +5,11 @@ import configs
 
 
 def dev_test():
-    url = 'http://dict.hjenglish.com/jp/jc/%E5%AF%9D%E3%82%8B'
+    url = 'http://dict.hjenglish.com/jp/jc/寝ない'
     content = requests.get(url, headers=configs.headers).content.decode('utf-8')
     doc = bs(content, 'html.parser')
-    print(doc)
+    print(doc.find_all(id='jpword_1')[0].text)
+    print(doc.find_all(id='kana_1')[0].text[1:-1])
 
 
 if __name__ == '__main__':
