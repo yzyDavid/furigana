129c65520c0e47ed0fe83bacdebdaf36b57ebaf0
yzyDavid <yzyDavid@qq.com>
2017-02-04 19:17:41 +0000

    added fake ua

--- src/furigana/dev.py
+++ src/furigana/dev.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+import configs
+
 
 def dev_test():
     url = 'http://dict.hjenglish.com/jp/jc/%E5%AF%9D%E3%82%8B'
-    content = requests.get(url).content.decode('utf-8')
+    content = requests.get(url, headers=configs.headers).content.decode('utf-8')
     doc = bs(content, 'html.parser')
     print(doc)
 
